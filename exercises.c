1b1c022859422c922e179f05c6bed35313571850
Replit user <>
2023-08-17 12:21:11 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -129,7 +129,30 @@ Descripción: Escribe una función que tome un arreglo y su tamaño,
 y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
-int checkSorted(int arr[], int size) { return -2; }
+int checkSorted(int arr[], int size) 
+{
+    int sube = 1;
+    int baja = 1;
+
+    for (int i = 1; i < size; i++) 
+    {
+        if (arr[i] < arr[i - 1]) 
+            sube = 0;
+
+        if (arr[i] > arr[i - 1]) 
+            baja = 0;
+    }
+
+    if (sube) 
+        return 1;
+        
+    else if (baja) 
+        return -1;
+
+    else 
+        return 0;
+}
+
 
 /*
 Ejercicio 6: Información de una Biblioteca
